61b24930b7a7aa22c3c2d93607c73b5f78987fdd
drewdeneke <70447289+drewdeneke@users.noreply.github.com>
2020-09-28 22:59:20 -0700

    Update AwwGeezMan.cpp

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -1,48 +1,83 @@
-/*
- _____ ____ _____   ____ _____ ____  
-| ____/ ___| ____| |___ \___  | ___| 
-|  _|| |   |  _|     __) | / /|___ \ 
-| |__| |___| |___   / __/ / /  ___) |
-|_____\____|_____| |_____/_/  |____/ 
-                                     
- _   _                                         _      _ 
-| | | | ___  _ __ ___   _____      _____  _ __| | __ / |
-| |_| |/ _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |
-|  _  | (_) | | | | | |  __/\ V  V / (_) | |  |   <  | |
-|_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\ |_|
- 
- 
-Aww Geez Man: Interdimensional Mortys.
-
-One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there
-exists a Morty who says everything backwards.
-
-*/
-
-// Include the Morty header file
+//Author: Drew Deneke
 #include "Morty.hpp"
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int first, char** second) {
 
 
-int main (int ac, char** av) {
-	// Parse the command line arguments. The program is executed as
-	// ./AwwGeezMan {start} {stop} {dimension}
-	// or 
-	// ./AwwGeezMan {start} {stop} {step} {dimension}
-	if (ac != 4 and ac != 5) {
-		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" 
-			<< std::endl;
-		std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
-		std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
-		
-		return -1;
-	}
-	
-	// Parse the command line arguments
-	
-		
-	// Depending on the dimension of the arguments, call the appropriate Morty
-	// function
-	
-	
-	return 0;
+    if (first != 4 and first != 5) {
+
+        std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)"
+            << std::endl;
+
+        std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
+
+        std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
+
+        return -1;
+    }
+
+
+
+    int start = atoi(second[1]), stop = atoi(second[2]), step = 1;//start stop are characters, transform to integers
+
+    std::string dimension; //declare string w unknown size
+
+    if (first == 4) {// 4 cmd arg passed
+
+        dimension = second[first - 1]; //length of string
+
+    }
+
+    else { // 5 cmd arg passed
+
+        step = atoi(second[3]); //step from char to int
+
+
+        dimension = second[first - 1];//length of string
+    }
+
+
+
+    if (dimension == "Z286") {
+        if (step == 1) {
+            Z286::Morty(start, stop);//function call for special case
+
+        }
+        else {
+
+            Z286::Morty(start, stop, step);//w stepping
+        }
+    }
+    else if (dimension == "C137") { // normal case
+
+        if (step == 1) {
+
+
+            C137::Morty(start, stop);// normal case
+
+        }
+
+        else {
+
+            C137::Morty(start, stop, step);// w stepping
+        }
+    }
+
+
+    else if (dimension != "C137" and dimension != "Z286") {// unknown dimension
+
+        std::cout << "ERROR: Unknown dimension!!" << std::endl;
+
+
+    }
+
+    return 0;
+
 }
