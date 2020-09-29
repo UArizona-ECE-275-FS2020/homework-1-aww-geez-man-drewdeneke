850a821e3f52b2b4b82923c67b90a8d792d130da
drewdeneke <70447289+drewdeneke@users.noreply.github.com>
2020-09-28 23:00:04 -0700

    Update Morty.cpp

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -1,21 +1,124 @@
-/* 
- __  __            _                                
-|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \ 
-| |  | | (_) | |  | |_| |_| |  _  | (__| |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/ 
-                       |___/           |_|   |_|  
-*/
-
-// Include the Morty header file
+//Author: Drew Deneke
 #include "Morty.hpp"
+#include<stdio.h>
+#include<string.h>
+#include<iostream>
 
-// =========================================================================== // 
-//          Define functions enclosed in Namespace C137 here 
-// =========================================================================== // 
+namespace C137 {
+
+	void Morty(int start, int stop, int step) {
+
+
+		std::cout << "Morty C137 says:" << std::endl;
+
+
+		for (int i = start; i <= stop; i = i + step) {
+			std::cout << i << ": ";
+
+			if (i % 3 == 0) {
+				if (i % 5 == 0) {
+
+					std::cout << "Aww Geez Man";//mult 15
+				}
+				else//mult 3
+					std::cout << "Aww";
+			}
+			else if (i % 5 == 0) { //mult 5
+
+				std::cout << "Geez";
+			}
+			std::cout << std::endl;
+		}
+	}
 
 
 
-// =========================================================================== // 
-//          Define functions enclosed in Namespace Z286 here 
-// =========================================================================== // 
+	void Morty(int start, int stop) {// function to loop from start to stop , stepping by 1
+
+
+		std::cout << "Morty C137 says:" << std::endl;
+
+
+		for (int i = start; i <= stop; i++) {
+
+
+			std::cout << i << ": ";
+
+			if (i % 3 == 0) {
+				if (i % 5 == 0) {
+
+					std::cout << "Aww Geez Man";//mult 15
+				}
+
+				else
+					std::cout << "Aww";
+			}
+
+			else if (i % 5 == 0) {//mult 5
+				std::cout << "Geez";
+			}
+			std::cout << std::endl;
+		}
+	}
+}
+
+
+namespace Z286 { // function to loop from start to stop, stepping by 1
+
+	void Morty(int start, int stop, int step) {
+
+
+		std::cout << "Morty Z286 says:" << std::endl;
+
+		// loop from start to stop, stepping in increments of 1
+
+		for (int i = start; i <= stop; i = i + step) {
+
+			std::cout << i << ": ";
+
+			if (i % 3 == 0) {
+
+				if (i % 5 == 0) {//mult 15
+
+
+					std::cout << "naM zeeG wwA";
+				}
+
+				else//mult 3
+					std::cout << "wwA";
+			}
+			else if (i % 5 == 0) {//mult 5
+
+				std::cout << "zeeG";
+			}
+
+			std::cout << std::endl;
+		}
+	}
+
+	// function to loop from start to stop in steps of 1
+	void Morty(int start, int stop)
+	{
+		std::cout << "Person Z286 says:" << std::endl;
+
+		for (int i = start; i <= stop; i++)
+		{
+			std::cout << i << ": ";
+			if (i % 3 == 0)
+			{
+				if (i % 5 == 0)// mult 15
+				{
+
+					std::cout << "naM zeeG wwA";
+				}
+				else//mult 3
+					std::cout << "wwA";
+			}
+			else if (i % 5 == 0) //mult 5
+			{
+				std::cout << "zeeG";
+			}
+			std::cout << std::endl;
+		}
+	}
+}
